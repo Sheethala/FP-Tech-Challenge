31ab6fbcffef8bb2e470117a1f7ce364e69d7f7e
Sheethala Swaminathan <sheethala@Sheethalas-MBP.lan>
2016-06-03 21:38:07 -0400

    Modified Files
    -Created routes
    -Added CSS

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,5 +4,96 @@
 angular.module('myApp.controllers', []).
 	controller('AppCtrl', function ($scope, $http) {
 		// Insert controller code here
-		
+		$scope.test = "Hello";
+		$scope.finalPrice = 0;
+		$scope.itemPrice = 0
+		console.log("calling the controller here");
+
+		$scope.splitStrings = function(Arr){
+				return Arr.split(';');
+		}
+
+		$scope.getSizeColorCode = function(Arr, style){
+			var length = Arr.length;
+			for(var i = 0; i<length; i++){
+				if(Arr[i].indexOf(style) != -1){
+					//We know that the colour exists
+					var colors = Arr[i].split(':');
+					return colors[0];
+				}
+			}
+		}
+
+		$scope.getQuote = function(){
+			var apparel_data;
+			var url = '/api/apparel/' + $scope.style_code;
+			var color_data;
+			var size_data;
+			var color;
+			var size;
+			var quote_object;
+			var weight;
+			console.log($scope.style_code);
+			$http.get(url).then(function(apparel, status) {
+				apparel_data = apparel.data;
+				if(apparel_data.length === 0){
+					swal("Oops", "Please check your values and try again", "error");
+				}
+				console.log(apparel.data);
+				color_data = $scope.splitStrings(apparel_data[0].color_codes);
+				color = $scope.getSizeColorCode(color_data, $scope.style_color);
+				size_data = $scope.splitStrings(apparel_data[0].size_codes);
+				size = $scope.getSizeColorCode(size_data, $scope.style_size);
+				weight = apparel_data[0].weight;
+				quote_object = {style_code: $scope.style_code, color_code: color, size_code: size };
+				$scope.sendData(quote_object, weight);
+				console.log(status);
+			});
+			console.log("THE QUOTE OBJECT IS")
+			console.log(quote_object);
+		}
+
+		$scope.calculatePrice = function(price, weight, quantity){
+				var final_price;
+				console.log(price, weight, quantity);
+				if(weight <= 0.4){
+					if(quantity < 48){
+						final_price = (price * quantity) + quantity;
+					}
+					else{
+						final_price = (price * quantity) + (0.75 * quantity);
+					}
+				}
+				else{
+					if(quantity < 48){
+						final_price = (price * quantity) + (0.5 * quantity);
+					}
+					else{
+						final_price = (price * quantity) + (0.25 * quantity);
+					}
+				}
+
+				//Create a salesman markup
+				final_price = 1.07 * final_price;
+				if(final_price <= 800){
+					final_price = 1.5 * final_price;
+				}
+				else{
+					final_price = 1.45 * final_price;
+				}
+				$scope.finalPrice = final_price;
+				$scope.itemPrice = final_price/quantity;
+				console.log(final_price);
+		}
+
+		$scope.sendData = function(obj, weight){
+			console.log("INSIDE THE SENDDATA FN");
+			$http.post('/api/quote', obj).then(function(price, status){
+				console.log("THE PRICE IS");
+				console.log(price.data);
+				console.log(typeof(price.data));
+				$scope.calculatePrice(Number(price.data), weight, Number($scope.quantity));
+			});
+		}
+
 	});
--- routes/api.js
+++ routes/api.js
@@ -7,30 +7,56 @@ var q           = require('q');
 var api         = express.Router();
 var db          = require('../config/db').connection;
 
+//Let the user enter brand, size and colour
+//Get the values from the database table, and call the merchandise api.
+
 // API endpoint for /api/apparel
 api.get('/api/apparel/:styleCode?', function(req, res) {
 	// Insert Apparel API code here
+
+  var style = req.param('styleCode');
+  var myQuery = "SELECT DISTINCT * from apparel WHERE style_code = '" + style + "'" ;
+  db.query(myQuery, function(err, rows, fields) {
+		if (err) throw err;
+		res.json(rows);
+	});
 
 });
 
 // API endpoint for /api/quote
 api.post('/api/quote', function(req, res) {
-	// Insert Quoting API code here
-
+  console.log("in the function");
+  console.log(req.body);
+  var myfr = getApparelPrice(req.body.style_code, req.body.color_code, req.body.size_code).then(function(result){
+    console.log(result);
+    res.send(result);
+  });
 });
 
 // Function for making an Inventory API call
 var getApparelPrice = function getPrice(style_code, color_code, size_code) {
-	var	apparelPriceDeferred = q.defer();
-
+	var apparelPriceDeferred = q.defer();
 	// Format the Inventory API endpoint as explained in the documentation
-	https.get('// INSERT INVENTORY API END POINT', function(res) {
+  var url = "https://www.alphashirt.com/cgi-bin/online/xml/inv-request.w?sr="+style_code+"&cc="+color_code+"&sc="+size_code+"&pr=y&zp=10002&userName=triggered1111&password=triggered2222";
+  console.log(url);
+	https.get(url, function(res) {
 		res.on('data', function (data) {
 			// Parse response XML data here
-
+      var parsed_data = data.toString();
+      console.log("The parsed data is >>>>>>>>>>>");
+      console.log(parsed_data);
+      var price_index = parsed_data.indexOf("price") + 8;
+      console.log(price_index);
+      var last_price_index = parsed_data.indexOf('"', price_index);
+      console.log(last_price_index);
+      var price = parsed_data.substring(price_index, last_price_index);
+      apparelPriceDeferred.resolve(price);
+      //Creates multiple requests without this line.
+      res.destroy();
 		});
 	}).on('error', function(error) {
 		// Handle EDI call errors here
+    apparelPriceDeferred.reject('Cannot find item');
 
 	});
 
